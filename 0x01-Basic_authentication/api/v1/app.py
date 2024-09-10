871ebebd3ce63916ecde96fed7f9dfde84730576
bikriira <iradufashabikri@gmail.com>
2024-09-10 18:23:48 +0200

    [Fix] only add checks if the request.path require auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -27,11 +27,10 @@ def before_request():
     if auth is None:
         pass
     if auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']):
-        pass
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 @app.errorhandler(404)
 def not_found(error) -> str:
