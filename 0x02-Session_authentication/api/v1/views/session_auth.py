b02bcf740aa05c5379fcb59d2eb9f4ff65327759
bikriira <iradufashabikri@gmail.com>
2024-10-11 18:13:49 +0200

    [Feature] implement authentication based on a Session ID stored in cookie, perfect for a website (browsers love cookies).

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,42 @@
+from api.v1.views import app_views
+from flask import request, jsonify, make_response
+from models.user import User
+import os
+
+
+@app_views.route("/auth_session/login", methods=["POST"], strict_slashes=False)
+def login():
+    """Handles user login and session creation.
+
+    This route allows a user to log in by providing their email
+    and password. Upon successful authentication, a session ID
+    is created, and the session cookie is set in the response.
+
+    Args:
+        None
+
+    Returns:
+        Response: A JSON response containing user information or
+                  error messages with appropriate HTTP status codes.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    user = User.search({"email": email})[0]
+
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+
+    session_id = auth.create_session(user.id)
+    response = make_response(user.to_json())
+    response.set_cookie(os.getenv("SESSION_NAME"), session_id)
+    return response
