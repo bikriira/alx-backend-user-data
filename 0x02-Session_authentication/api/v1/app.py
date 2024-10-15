0bde92aa80eebd7af46c2717ce0d928d1358b828
bikriira <iradufashabikri@gmail.com>
2024-10-15 12:25:38 +0200

    [Fix] Add a chebang on top of file, and Docstrings

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 import os
 
 
@@ -24,6 +25,8 @@ if auth_type == "basic_auth":
     auth = BasicAuth()
 if auth_type == "session_auth":
     auth = SessionAuth()
+if auth_type == "session_exp_auth":
+    auth = SessionExpAuth()
 
 
 @app.before_request
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,10 +1,20 @@
+#!/usr/bin/env python3
+"""
+Defines a User model for SQLAlchemy with fields for user information.
+
+Classes:
+    User: A table representing users, including email, hashed password,
+          session ID, and reset token.
+"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
 class User(Base):
+    """SQLAlchemy User model."""
+    
     __tablename__ = "users"
 
     id = Column(Integer(), primary_key=True)
